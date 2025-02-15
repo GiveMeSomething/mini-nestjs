0be024c88f3b6b49d3667b2f6a9ac714f345adb4
Hoang Tien Minh <htminh.1001@gmail.com>
2025-02-15 15:13:52 +0700

    chore: add logging support with Pino

--- src/internal/app.module.ts
+++ src/internal/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { DatabaseModule } from "./database/database.module";
+import { LoggerModule } from "nestjs-pino";
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [LoggerModule.forRoot(), DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
