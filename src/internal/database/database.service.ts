957633ebba0a641974a88b66649b4ba6c9c60924
Hoang Tien Minh <htminh.1001@gmail.com>
2025-02-16 21:39:10 +0700

    fix: close dangling connection to database and redis when server shutdown

--- src/internal/database/database.service.ts
+++ src/internal/database/database.service.ts
@@ -1,16 +1,20 @@
 import { createKnexClient, RawDatabaseConfig } from "@/config/database";
-import { Inject, Injectable } from "@nestjs/common";
+import { Inject, Injectable, OnApplicationShutdown } from "@nestjs/common";
 import { Knex } from "knex";
 import { AppConfig } from "@/config/config";
 import { APP_CONFIG } from "@/config/config.module";
 
 @Injectable()
-export class DatabaseService {
+export class DatabaseService implements OnApplicationShutdown {
   readonly rawConfig: RawDatabaseConfig;
   readonly database: Knex;
 
   constructor(@Inject(APP_CONFIG) appConfig: AppConfig) {
     this.rawConfig = appConfig.database;
     this.database = createKnexClient(appConfig.database);
+  }
+
+  async onApplicationShutdown(signal?: string) {
+    await this.database.destroy();
   }
 }
