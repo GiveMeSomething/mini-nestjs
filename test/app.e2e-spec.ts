d0254f4c7ab0f817cf8d1f279c5eb89f1b46180e
Hoang Tien Minh <htminh.1001@gmail.com>
2025-02-16 21:38:52 +0700

    fix: update jest e2e config to be able to run e2e testing on server

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,22 +2,28 @@ import { Test, TestingModule } from "@nestjs/testing";
 import { INestApplication } from "@nestjs/common";
 import { App } from "supertest/types";
 import { AppModule } from "@/internal/app.module";
-import request from "supertest";
+import { agent } from "supertest";
 
 describe("AppController (e2e)", () => {
   let app: INestApplication<App>;
+  let module: TestingModule;
 
   beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
+    module = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
-    app = moduleFixture.createNestApplication();
+    app = module.createNestApplication();
     await app.init();
   });
 
-  it("/ (GET)", () => {
-    return request(app.getHttpServer())
+  afterAll(async () => {
+    await app.close();
+    await module.close();
+  });
+
+  it("/health_check (GET)", () => {
+    return agent(app.getHttpServer())
       .get("/health_check")
       .expect(200)
       .expect("healthy");
